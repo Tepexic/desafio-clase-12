f60e18e43c2adf52b396b5acc415b64dbd573c3a
Jesus6Connex <jesus.velazquez@6connex.com>
2021-10-24 15:19:11 -0500

    add function to emit messages and handle new messages

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,24 @@ const socket = io.connect();
 
 // Obtener plantilla de handlebars
 const tablaProductosSrc = document.getElementById("product-table").innerHTML;
+const messageLogSrc = document.getElementById("message-log").innerHTML;
 
 // función para renderizar tabla de productos, pasando la lista a la plantilla compilada
 // de handlebars
 socket.on("productos", (data) => {
   console.log(data);
-  var template = Handlebars.compile(tablaProductosSrc);
+  const template = Handlebars.compile(tablaProductosSrc);
   const productos = { productos: data };
   document.getElementById("table-placeholder").innerHTML = template(productos);
+});
+
+// función para renderizar tabla de mensajes
+socket.on("messages", (data) => {
+  console.log(data);
+  const template = Handlebars.compile(messageLogSrc);
+  const mensajes = { messages: data };
+  document.getElementById("messages-placeholder").innerHTML =
+    template(mensajes);
 });
 
 // función para mandar la información del formulario como mensaje
@@ -27,5 +37,20 @@ function addProduct() {
   title.value = "";
   price.value = "";
   thumbnail.value = "";
+  return false;
+}
+
+// Función para mandar mensajes al servidor
+function addMessage() {
+  const account = document.getElementById("account");
+  const timestamp = new Date().toLocaleString();
+  const message = document.getElementById("message");
+  const mensaje = {
+    account: account.value,
+    timestamp,
+    text: message.value,
+  };
+  socket.emit("new-message", mensaje);
+  message.value = ""; // limpiar campo de mensaje
   return false;
 }
