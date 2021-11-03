2ff2bc701d4f4e1d729babfe21229638414039b9
Jesus6Connex <jesus.velazquez@6connex.com>
2021-11-02 19:05:50 -0600

    create products database

---
+++ DB/products-table/createTable.js
@@ -0,0 +1,20 @@
+const { options } = require("./options/mysql");
+const knex = require("knex")(options);
+
+knex.schema
+  .createTable("products", (table) => {
+    table.increments("id");
+    table.string("title");
+    table.integer("price");
+    table.string("thumbnail");
+  })
+  .then(() => {
+    console.log("Table created!");
+  })
+  .catch((err) => {
+    console.error(err);
+    throw err;
+  })
+  .finally(() => {
+    knex.destroy();
+  });
