8f344fba32e6dc04d97e8d8944ec8598add85fbd
Jesus6Connex <jebus.velazquez@gmail.com>
2021-11-30 20:56:49 -0600

    add route to get  faker products

--- routes/productos.js
+++ routes/productos.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const faker = require("faker");
 const { Router } = express;
 
 const Contenedor = require("./../utils/Contenedor");
@@ -17,6 +18,20 @@ productosRouter.post("/", async (req, res) => {
   const productoNuevo = req.body;
   await Productos.save(productoNuevo);
   res.redirect(301, "../");
+});
+
+productosRouter.get("/productos-test", (req, res) => {
+  const productos = [
+    ...new Array(10).fill(0).map((_, i) => {
+      return {
+        id: i,
+        title: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        thumbnail: faker.image.imageUrl(),
+      };
+    }),
+  ];
+  res.json(productos);
 });
 
 module.exports = productosRouter;
